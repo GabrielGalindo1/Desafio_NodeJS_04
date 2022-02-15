18553a58d724c69867f0954c7f84126f4b398769
GabrielGalindo1 <gabrieldelimagalindo.gdlg@gmail.com>
2022-02-15 17:02:44 -0300

    Foi criado um readline para o usuário poder realizar o input das informações, criado a classe aluno com seus atributos e o array listaDeAlunos, dentro do array foi usado um .map e .reduce para realizar a soma das notas dos alunos.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { rawListeners } from "process";
 
 dotenv.config();
 
@@ -32,7 +33,11 @@ app.use(express.json());
 /**
  * Server Activation
  */
-
+	class Aluno{
+		nome = '';
+		idade = 0;
+		nota = 0;
+	}
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 
@@ -40,4 +45,21 @@ app.listen(PORT, () => {
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
 	
+	const readline = require('readline-sync')
+
+	const qntAlunos = 3;
+
+	let i =0;
+	let listaDeAlunos: Aluno [] = [];
+		
+	for(i= 0; i < qntAlunos; i++){
+	const aluno = new Aluno();
+		 aluno.nome = readline.question('Nome do Aluno?\n')
+		 aluno.idade = readline.question('Idade do Aluno?\n')
+		 aluno.nota = parseInt (readline.question('Nota do Aluno?\n'))
+		 listaDeAlunos.push(aluno)
+		 console.log(listaDeAlunos)	
+	}
+	const sumall = listaDeAlunos.map(item => item.nota).reduce((prev, curr) => prev + curr, 0);
+	console.log('A soma das notas e:',sumall);
 });
